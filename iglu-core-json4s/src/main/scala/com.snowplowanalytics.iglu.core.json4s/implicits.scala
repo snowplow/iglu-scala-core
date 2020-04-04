5e7332be332a328fa3de1162bdd81b3a4adddc19
Dilyan Damyanov <dilyand@gmail.com>
2020-04-04 17:56:00 +0100

    Iglu Core Json4s: remove implicits.scala and rename instances.scala to implicits.scala (close #57)

--- iglu-core-json4s/src/main/scala/com.snowplowanalytics.iglu.core.json4s/implicits.scala
+++ iglu-core-json4s/src/main/scala/com.snowplowanalytics.iglu.core.json4s/implicits.scala
@@ -13,4 +13,77 @@
 package com.snowplowanalytics.iglu.core
 package json4s
 
-object implicits extends instances
+import org.json4s._
+import org.json4s.JsonDSL._
+import org.json4s.jackson.JsonMethods.compact
+
+import com.snowplowanalytics.iglu.core.typeclasses._
+
+trait implicits {
+
+  private implicit val codecs = Json4sIgluCodecs.formats
+
+  final implicit val igluAttachToDataJValue: ExtractSchemaKey[JValue] with ToData[JValue] =
+    new ExtractSchemaKey[JValue] with ToData[JValue] {
+
+      def extractSchemaKey(entity: JValue) =
+        entity \ "schema" match {
+          case JString(schema) => SchemaKey.fromUri(schema)
+          case _               => Left(ParseError.InvalidData)
+        }
+
+      def getContent(json: JValue): Either[ParseError, JValue] =
+        json \ "data" match {
+          case JNothing => Left(ParseError.InvalidData)
+          case data => Right(data)
+        }
+    }
+
+  final implicit val igluAttachToSchema: ExtractSchemaMap[JValue] with ToSchema[JValue] with ExtractSchemaMap[JValue] =
+    new ToSchema[JValue] with ExtractSchemaMap[JValue] {
+
+      def extractSchemaMap(entity: JValue): Either[ParseError, SchemaMap] =
+        (entity \ "self").extractOpt[SchemaKey].map(key => SchemaMap(key)) match {
+          case Some(map) => Right(map)
+          case None => Left(ParseError.InvalidSchema)
+        }
+
+      def checkSchemaUri(entity: JValue): Either[ParseError, Unit] = {
+        (entity \ "$schema").extractOpt[String] match {
+          case Some(schemaUri) if schemaUri ==  SelfDescribingSchema.SelfDescribingUri.toString => Right(())
+          case _ => Left(ParseError.InvalidMetaschema)
+        }
+      }
+
+      def getContent(schema: JValue): JValue =
+        Json4sIgluCodecs.removeMetaFields(schema)
+    }
+
+  // Container-specific instances
+
+  final implicit val igluNormalizeDataJValue: NormalizeData[JValue] =
+    new NormalizeData[JValue] {
+      def normalize(instance: SelfDescribingData[JValue]): JValue =
+        Extraction.decompose(instance)
+    }
+
+  final implicit val igluNormalizeSchemaJValue: NormalizeSchema[JValue] =
+    new NormalizeSchema[JValue] {
+      def normalize(schema: SelfDescribingSchema[JValue]): JValue =
+        Extraction.decompose(schema)
+    }
+
+  final implicit val igluStringifyDataJValue: StringifyData[JValue] =
+    new StringifyData[JValue] {
+      def asString(container: SelfDescribingData[JValue]): String =
+        compact(container.normalize(igluNormalizeDataJValue))
+    }
+
+  final implicit val igluStringifySchemaJValue: StringifySchema[JValue] =
+    new StringifySchema[JValue] {
+      def asString(container: SelfDescribingSchema[JValue]): String =
+        compact(container.normalize(igluNormalizeSchemaJValue))
+    }
+}
+
+object implicits extends implicits
