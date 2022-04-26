16837eac964302cc2d2302d8bae91b24b0a4dc27
Ian Streeter <ian@snowplowanalytics.com>
2022-04-26 22:55:03 +0100

    Add Json Schema V4 text (close #87)

---
+++ iglu-core-circe/src/test/scala/com/snowplowanalytics/iglu/core/circe/MetaSchemasSpec.scala
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2016-2022 Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package com.snowplowanalytics.iglu.core.circe
+
+import io.circe.parser.parse
+
+import org.specs2.Specification
+
+class MetaSchemasSpec extends Specification {
+  def is = s2"""
+    JsonSchema must be valid JSON $e1
+  """
+
+  def e1 =
+    parse(MetaSchemas.JsonSchemaV4Text) must beRight
+}
